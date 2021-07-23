67b9a94f7f915c438d0a115b8b7999f164c3808c
jarrellrn <jarrellrn@gmail.com>
2021-07-23 15:35:40 -0500

    added program options, parsing

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const program = require("commander")
 program.version("1.0.0").description("Simple Password Generator")
 
 program
-  .command("generate")
-  .action(() => {
-    console.log("Generated.")
-  })
+  .option("-l, --length <number>", "length of password", "8")
+  .option("-s, --save", "save password to passwords.txt")
+  .option("-nn, --no-numbers", "remove numbers")
+  .option("-ns, --no-symbols", "remove symbols")
   .parse()
 
+const {length, save, numbers, symbols} = program.opts()
+
+// Get generated password
+const generatedPassword = createPassword()
